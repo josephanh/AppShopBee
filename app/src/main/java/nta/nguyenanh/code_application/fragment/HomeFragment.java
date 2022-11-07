503858bfca693ea29161550612924806da0f31a1
NguyenAnh003 <teamprojectnta@gmail.com>
2022-11-07 23:04:57 +0700

    update_7

--- app/src/main/java/nta/nguyenanh/code_application/fragment/HomeFragment.java
+++ app/src/main/java/nta/nguyenanh/code_application/fragment/HomeFragment.java
@@ -7,12 +7,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import nta.nguyenanh.code_application.R;
 import nta.nguyenanh.code_application.adapter.PhotoAdapter;
@@ -22,6 +26,10 @@ public class HomeFragment extends Fragment {
 
     ViewPager viewPager;
     PhotoAdapter photoAdapter;
+
+    List<Photo_banner> listPhoto;
+
+    Timer timer;
 
 
     public HomeFragment() {
@@ -54,8 +62,13 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         viewPager = view.findViewById(R.id.viewpageHome);
 
-        photoAdapter = new PhotoAdapter(getContext(), listBanner());
+        listPhoto = listBanner();
+
+        photoAdapter = new PhotoAdapter(getContext(), listPhoto);
         viewPager.setAdapter(photoAdapter);
+        autoSlideBanner();
+
+
     }
 
     private List<Photo_banner> listBanner() {
@@ -66,5 +79,44 @@ public class HomeFragment extends Fragment {
         list.add(new Photo_banner(R.drawable.banner_4));
         list.add(new Photo_banner(R.drawable.banner_5));
         return list;
+    }
+
+    private void autoSlideBanner() {
+        if(listPhoto == null || listPhoto.isEmpty() || viewPager == null) {
+            return;
+        }
+        // Init timer
+        if(timer == null) {
+            timer = new Timer();
+        }
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int currentItem = viewPager.getCurrentItem();
+                        int totalItem = listPhoto.size() -1;
+
+                        if(currentItem < totalItem) {
+                            currentItem ++;
+                            viewPager.setCurrentItem(currentItem);
+                        } else {
+                            viewPager.setCurrentItem(0);
+                        }
+                    }
+                });
+            }
+        }, 500, 3000);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(timer != null) {
+            timer.cancel();
+            timer = null;
+        }
     }
 }
