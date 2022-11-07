d5c203150e98afd11bdc40611a2d6a54c37805a2
NguyenAnh003 <teamprojectnta@gmail.com>
2022-11-07 17:46:15 +0700

    update_3

--- app/src/main/java/nta/nguyenanh/code_application/MainActivity.java
+++ app/src/main/java/nta/nguyenanh/code_application/MainActivity.java
@@ -3,15 +3,21 @@ package nta.nguyenanh.code_application;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    FrameLayout frameLayout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar;
+        frameLayout = findViewById(R.id.framelayout);
+
+
     }
 }
