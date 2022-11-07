d0d124a300c654e4fe0204f8b6d9c3c6b73af120
NguyenAnh003 <teamprojectnta@gmail.com>
2022-11-07 18:14:51 +0700

    update_4

--- app/src/main/java/nta/nguyenanh/code_application/MainActivity.java
+++ app/src/main/java/nta/nguyenanh/code_application/MainActivity.java
@@ -2,22 +2,24 @@ package nta.nguyenanh.code_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
+import nta.nguyenanh.code_application.fragment.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    FrameLayout frameLayout;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        frameLayout = findViewById(R.id.framelayout);
-
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.framelayout, homeFragment).commit();
 
     }
 }
