57820824209b57eacb47c1b4be12c91f75b83641
leviettinh1479 <115515237+leviettinh1479@users.noreply.github.com>
2022-11-12 22:24:43 +0700

    update_8
    xóa 1 item

--- app/src/main/java/nta/nguyenanh/code_application/Adapter_History.java
+++ app/src/main/java/nta/nguyenanh/code_application/Adapter_History.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,17 +30,28 @@ public class Adapter_History extends RecyclerView.Adapter<Adapter_History.ViewHo
     @Override
     public void onBindViewHolder(@NonNull Adapter_History.ViewHolder holder, int position) {
         holder.txt_name_history.setText(ds.get(position).getName_history());
+        holder.MaNews = ds.get(position).getId_history();
+        holder.txt_delete_item_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DAO_History dh =new DAO_History(context);
+                dh.delete(holder.MaNews+"");
+                ds= dh.getAll();
+                notifyDataSetChanged();
+            }
+        });
     }
     @Override
     public int getItemCount() {
         return ds.size();
     }
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView txt_name_history;
+        public TextView txt_name_history,txt_delete_item_history;
         int MaNews;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             txt_name_history =itemView.findViewById(R.id.txt_item_history);
+            txt_delete_item_history = itemView.findViewById(R.id.txt_delete_item_history);
         }
     }
 }
