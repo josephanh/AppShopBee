af6f2b97801e31a1c5aac7f9bfd62db9ec9831e8
NguyenAnh003 <teamprojectnta@gmail.com>
2022-11-16 22:46:32 +0700

    update_14

--- app/src/main/java/nta/nguyenanh/code_application/fragment/HomeFragment.java
+++ app/src/main/java/nta/nguyenanh/code_application/fragment/HomeFragment.java
@@ -133,7 +133,7 @@ public class HomeFragment extends Fragment {
                 Point size = new Point();
                 display.getSize(size);
 
-                toolbar_0.setAlpha((float) (1 - scrollY*0.003));
+                toolbar.setAlpha((float) (scrollY*0.003));
 
                 if(scrollY < recyclerView_product.getHeight()) {
                     recyclerView_product.setNestedScrollingEnabled(false);
