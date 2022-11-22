e801f4b8fa0c7a646879e6c1ebb65c5cd04938e0
NguyenAnh003 <teamprojectnta@gmail.com>
2022-11-23 02:50:32 +0700

    update_19

--- app/src/main/java/nta/nguyenanh/code_application/AddressActivity.java
+++ app/src/main/java/nta/nguyenanh/code_application/AddressActivity.java
@@ -1,5 +1,8 @@
 package nta.nguyenanh.code_application;
 
+import static nta.nguyenanh.code_application.MainActivity.userModel;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -7,7 +10,16 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class AddressActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     Toolbar toolbar;
     @SuppressLint("MissingInflatedId")
@@ -25,6 +37,29 @@ public class AddressActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        addAddress();
 
+    }
+
+    public void addAddress() {
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("name", "name");
+        hashMap.put("city", "địa chỉ nhận hàng");
+
+        db.collection("user").document(userModel.getUserID())
+                .update("address",hashMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 }
