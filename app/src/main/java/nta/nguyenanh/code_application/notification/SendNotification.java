b8cecef595ab69f68e5cf84f260c147d6ebce62c
tnmod <tnmod2003@gmail.com>
2022-11-27 15:34:36 +0700

    UPDATE 27-11-2022

--- app/src/main/java/nta/nguyenanh/code_application/notification/SendNotification.java
+++ app/src/main/java/nta/nguyenanh/code_application/notification/SendNotification.java
@@ -2,13 +2,12 @@ package nta.nguyenanh.code_application.notification;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
-import static com.facebook.FacebookSdk.getApplicationContext;
-
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -18,15 +17,14 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.widget.RemoteViews;
 
 import nta.nguyenanh.code_application.R;
 
-public class Notification {
+public class SendNotification {
 
     Context context;
 
-    public Notification(Context context) {
+    public SendNotification(Context context) {
         this.context = context;
     }
 
@@ -48,34 +46,46 @@ public class Notification {
     public void customNotification(String subText, String contentText){
 
         NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-        android.app.Notification notification;
+        Notification notification;
 
-        Intent iNotify = new Intent(context, Notification.class);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Utils.CHANNEL_ID);
+
+        Intent iNotify = new Intent(context, SendNotification.class);
         iNotify.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, Utils.REQ_CODE, iNotify, PendingIntent.FLAG_IMMUTABLE);
 
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
-                    notification = new android.app.Notification.Builder(context)
+                    notification = new NotificationCompat.Builder(context, Utils.CHANNEL_ID)
                             .setSmallIcon(R.drawable.logo_1)
                             .setContentText(contentText)
 //                            .setLargeIcon(getBitmapFromVectorDrawable(this, R.drawable.ic_logo_main))
                             .setSubText(subText)
                             .setSettingsText(subText)
                             .setChannelId(Utils.CHANNEL_ID)
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText(contentText))
                             .build();
                     manager.createNotificationChannel(new NotificationChannel(Utils.CHANNEL_ID, "Shopbee", NotificationManager.IMPORTANCE_HIGH));
+                    manager.notify(Utils.NOTIFY_ID, notification);
         }
         else {
-                notification = new android.app.Notification.Builder(context)
-                        .setSmallIcon(R.drawable.logo_1)
-                        .setContentText(contentText)
-                        .setSubText(subText)
-//                        .setLargeIcon(getBitmapFromVectorDrawable(this, R.drawable.ic_logo_main))
-                        .setContentIntent(pendingIntent)
-                        .build();
+                    builder.setSmallIcon(R.drawable.logo_1)
+                            .setContentTitle(subText)
+                            .setContentText(contentText)
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setAutoCancel(true)
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText(contentText));
+                    NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+                    notificationManagerCompat.notify(Utils.NOTI_ID, builder.build());
+//                notification = new android.app.SendNotification.Builder(context)
+//                        .setSmallIcon(R.drawable.logo_1)
+//                        .setContentText(contentText)
+//                        .setSubText(subText)
+////                        .setLargeIcon(getBitmapFromVectorDrawable(this, R.drawable.ic_logo_main))
+//                        .setContentIntent(pendingIntent)
+//                        .build();
         }
-        manager.notify(Utils.NOTIFY_ID, notification);
+
     }
 }
