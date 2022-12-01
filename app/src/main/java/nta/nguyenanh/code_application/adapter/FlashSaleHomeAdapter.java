eb301106f83045c788cbb83c9e2f00c3a19c4ef7
leviettinh1479 <115515237+leviettinh1479@users.noreply.github.com>
2022-12-01 19:38:30 +0700

    update_29

--- app/src/main/java/nta/nguyenanh/code_application/adapter/FlashSaleHomeAdapter.java
+++ app/src/main/java/nta/nguyenanh/code_application/adapter/FlashSaleHomeAdapter.java
@@ -1,10 +1,12 @@
 package nta.nguyenanh.code_application.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,7 +37,13 @@ public class FlashSaleHomeAdapter extends RecyclerView.Adapter<FlashSaleHomeAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHoler holder, int position) {
+        Double Count= holder.bar_tong.getLayoutParams().width*Math.pow(list.get(position).getTotal(),-1) ;
         Glide.with(context).load(list.get(position).getImage().get(0)).into(holder.image_product);
+        holder.price_product.setText("đ"+" "+list.get(position).getPrice()+"");
+        holder.sold.setText(list.get(position).getSold()+" ĐÃ BÁN");
+        ViewGroup.LayoutParams parm = holder.bar_daban.getLayoutParams();
+        parm.width = Integer.parseInt(Math.round(Count*list.get(position).getSold())+"");
+        holder.bar_daban.setLayoutParams(parm);
     }
 
     @Override
@@ -48,9 +56,14 @@ public class FlashSaleHomeAdapter extends RecyclerView.Adapter<FlashSaleHomeAdap
 
     protected class ViewHoler extends RecyclerView.ViewHolder {
         ImageView image_product;
+        TextView price_product,bar_tong,bar_daban,sold;
         public ViewHoler(@NonNull View itemView) {
             super(itemView);
             image_product = itemView.findViewById(R.id.image_product);
+            price_product = itemView.findViewById(R.id.price_product);
+            bar_tong = itemView.findViewById(R.id.bar_tong);
+            sold = itemView.findViewById(R.id.sold);
+            bar_daban = itemView.findViewById(R.id.bar_daban);
         }
     }
 }
